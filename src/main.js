5c0f5eb869418ca5fef4e4544aca78b8b98cb549
Paul Pessoa <paulmspessoa@gmail.com>
2022-05-20 17:39:46 -0300

    config firebase

--- src/main.js
+++ src/main.js
@@ -3,4 +3,22 @@ import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app'
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyCNMjuM8-JpowuK57jrFljyHV9s2-EBytY',
+  authDomain: 'auth-way.firebaseapp.com',
+  projectId: 'auth-way',
+  storageBucket: 'auth-way.appspot.com',
+  messagingSenderId: '99959402799',
+  appId: '1:99959402799:web:dc25a8f1185edda991f1da'
+}
+
+// Initialize Firebase
+initializeApp(firebaseConfig)
+
 createApp(App).use(router).mount('#app')
